3c531b680e7198511554802c112b38aa89389cc9
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-04-14 19:25:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,10 +13,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configs
-API_HASH = os.environ.get('1a7e01278b2bf0fe975fb8ce7c5ead8d') # Api hash
-APP_ID = int(os.environ.get('25298674')) # Api id/App id
-BOT_TOKEN = os.environ.get('7141112210:AAFLVbtxyU-BaGfzu4RyH97iCxVnjVwzt-Y') # Bot token
-OWNER_ID = os.environ.get('6793357832') # Your telegram id
+API_HASH = os.environ.get('61f8a38f9d9d079100ba56fcbb9ea9b7') # Api hash
+APP_ID = int(os.environ.get('26181533')) # Api id/App id
+BOT_TOKEN = os.environ.get('7072770670:AAEpSfBg6P5z89aSok-E_fMKNJCjqnOdOJk') # Bot token
+OWNER_ID = os.environ.get('6329158981') # Your telegram id
 AS_ZIP = bool(os.environ.get('AS_ZIP', False)) # Upload method. If True: will Zip all your files and send as zipfile | If False: will send file one by one
 BUTTONS = bool(os.environ.get('BUTTONS', False)) # Upload mode. If True: will send buttons (Zip or One by One) instead of AZ_ZIP | If False: will do as you've fill on AZ_ZIP
 
